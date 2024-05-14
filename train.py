83b39598f2ed99e03e03a3e30ae60a019d2b9e13
Huu Tuong Tu <83907151+huutuongtu@users.noreply.github.com>
2024-05-14 14:35:03 +0700

    Update train.py

--- train.py
+++ train.py
@@ -270,9 +270,9 @@ def main():
     parser.add_argument('--config', default='')
     parser.add_argument('--training_epochs', default=1000, type=int)
     parser.add_argument('--stdout_interval', default=5, type=int)
-    parser.add_argument('--checkpoint_interval', default=100, type=int)
+    parser.add_argument('--checkpoint_interval', default=10000, type=int)
     parser.add_argument('--summary_interval', default=100, type=int)
-    parser.add_argument('--validation_interval', default=100, type=int)
+    parser.add_argument('--validation_interval', default=2000, type=int)
     parser.add_argument('--fine_tuning', default=False, type=bool)
 
     a = parser.parse_args()
